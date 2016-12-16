af6f1aaaeaab903fe70e807b0a8643255f63c82b
PurwantoGz <purwanto@outlook.com>
2016-12-17 02:35:25 +0700

    Design Form

---
+++ OpticalCharacterRecognition/MainView.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MetroFramework.Forms;
+namespace OpticalCharacterRecognition
+{
+    public partial class MainView : MetroForm
+    {
+        OpenFileDialog ofd = new OpenFileDialog();
+        public MainView()
+        {
+            InitializeComponent();
+        }
+
+        private void OpenFile(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+            openFileDialog.Filter = "Text Files (*.txt)|*.txt|All Files (*.*)|*.*";
+            if (openFileDialog.ShowDialog(this) == DialogResult.OK)
+            {
+                string FileName = openFileDialog.FileName;
+            }
+        }
+
+        private void newImageTool_Click(object sender, EventArgs e)
+        {
+            using (ofd)
+            {
+                ofd.Title = "Open Image Training";
+                ofd.Filter = "Image files (*.jpg, *.jpeg, *.jpe, *.jfif, *.png) | *.jpg; *.jpeg; *.jpe; *.jfif; *.png";
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    OpenImageView openimage =new OpenImageView(ofd);
+                    newImageTool.Enabled = false;
+                    enabletool(true);
+                    OpentoolStripMenuItem.Enabled = false;
+                    openimage.MdiParent = this;
+                    openimage.FormClosed += new FormClosedEventHandler(openimageclosed);
+                    openimage.Show();
+                }
+            }
+        }
+
+        private void openimageclosed(object sender, FormClosedEventArgs e)
+        {
+            newImageTool.Enabled = true;
+            OpentoolStripMenuItem.Enabled = true;
+            enabletool(false);
+        }
+
+        private void OpentoolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            using (ofd)
+            {
+                ofd.Title = "Open Image Training";
+                ofd.Filter = "Image files (*.jpg, *.jpeg, *.jpe, *.jfif, *.png) | *.jpg; *.jpeg; *.jpe; *.jfif; *.png";
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    OpenImageView openimage = new OpenImageView(ofd);
+                    newImageTool.Enabled = false;
+                    enabletool(true);
+                    OpentoolStripMenuItem.Enabled = false;
+                    openimage.MdiParent = this;
+                    openimage.FormClosed += new FormClosedEventHandler(openimageclosed);
+                    openimage.Show();
+                }
+            }
+        }
+
+        private void toolSave2Pdf_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void toolSave2Text_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void toolZoomIn_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void toolZoomOut_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void toolRotate_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void toolConvert_Click(object sender, EventArgs e)
+        {
+            ConvertView convertview = new ConvertView();
+            convertview.MdiParent = this;
+            toolConvert.Enabled = false;
+            convertview.FormClosed += new FormClosedEventHandler(convertviewclosed);
+            convertview.Show();
+        }
+
+        private void convertviewclosed(object sender, FormClosedEventArgs e)
+        {
+            toolConvert.Enabled = true;
+        }
+
+        private void enabletool(bool state)
+        {
+            toolSave2Pdf.Enabled = state;
+            toolSave2Text.Enabled = state;
+            toolZoomIn.Enabled = state;
+            toolZoomOut.Enabled = state;
+            toolZoomOut.Enabled = state;
+            toolRotate.Enabled = state;
+            toolConvert.Enabled = state;
+        }
+    }
+}
